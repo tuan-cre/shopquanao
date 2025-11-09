d65d6057219457098db499974cd4f11145e6a4b1
tuan-cre <lhtuan@gmail.com>
2025-11-09 09:01:22 +0700

    change how login work

--- admin/ktnguoidung/profile.php
+++ admin/ktnguoidung/profile.php
@@ -1,53 +1,21 @@
 <?php include("../inc/top.php"); ?>
-<!-- Form cập nhật thông tin ng dùng-->
 <div class="row">
-    <div class="col-12 col-md-10 m-auto">
-        <div class="card p-5">
+    <div class="col-12 col-md-8 m-auto">
+        <div class="card p-4">
             <div class="card-header">
-                <h4 class="text-info text-center">HỒ SƠ NGƯỜI DÙNG</h4>
+                <h4 class="text-info text-center">HỒ SƠ TÀI KHOẢN</h4>
             </div>
             <div class="card-body">
-                <form method="post" enctype="multipart/form-data" action="../ktnguoidung/index.php">
-                    <input type="hidden" name="txtid" value="<?php echo
-                        $_SESSION["nguoidung"]["id"]; ?>">
-                    <input type="hidden" name="txthinhanh" value="<?php echo
-                        $_SESSION["nguoidung"]["hinhanh"]; ?>">
-                    <input type="hidden" name="action" value="xlhoso">
-                    <div class="text-center">
-                        <img class="img-thumbnail" src="<?php if
-                        ($_SESSION["nguoidung"]["hinhanh"] == NULL)
-                            echo "../../images/users/user.png";
-                        else
-                            echo
-                                "../../images/users/" . $_SESSION["nguoidung"]["hinhanh"]; ?>" alt="<?php echo
-                                   $_SESSION["nguoidung"]["hoten"]; ?>" width="100px">
-                    </div>
-                    <input type="hidden" name="txtid" value="<?php echo
-                        $_SESSION["nguoidung"]["id"]; ?>">
-                    <div class="my-3">
-                        <label class="form-label">Email</label>
-                        <input class="form-control" type="email" name="txtemail" placeholder="Email"
-                            value="<?php echo $_SESSION["nguoidung"]["email"]; ?>" required>
-                    </div>
-                    <div class="my-3">
-                        <label class="form-label">Số điện thoại</label>
-                        <input class="form-control" type="number" name="txtdienthoai" placeholder="Số
-điện thoại" value="<?php echo $_SESSION["nguoidung"]["sodienthoai"]; ?>" required>
-                    </div>
-                    <div class="my-3">
-                        <label class="form-label">Họ tên</label>
-                        <input class="form-control" type="text" name="txthoten" placeholder="Họ tên"
-                            value="<?php echo $_SESSION["nguoidung"]["hoten"]; ?>" required>
-                    </div>
-                    <div class="my-3">
-                        <label class="form-label">Đổi hình đại diện</label>
-                        <input class="form-control" type="file" name="fhinh">
-                    </div>
-                    <div class="my-3 text-center">
-                        <input class="btn btn-primary" type="submit" value="Cập nhật">
-                        <input class="btn btn-warning" type="reset" value="Không">
-                    </div>
-                </form>
+                <?php if (isset($_SESSION["nguoidung"])): 
+                    $u = $_SESSION["nguoidung"];
+                ?>
+                <p><strong>Username:</strong> <?php echo htmlspecialchars($u["nguoidung"]); ?></p>
+                <p><strong>Quyền:</strong> <?php echo ($u["loai"]==1) ? 'Admin' : (($u["loai"]==2)?'Nhân viên':'Khách hàng'); ?></p>
+                <p><strong>Trạng thái:</strong> <?php echo ($u["trangthai"]==1) ? 'Hoạt động' : 'Khóa'; ?></p>
+                <p><a class="btn btn-primary" href="index.php?action=matkhau">Đổi mật khẩu</a></p>
+                <?php else: ?>
+                <p>Chưa đăng nhập.</p>
+                <?php endif; ?>
             </div>
         </div>
     </div>
