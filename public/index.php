90bfb0798886de5ec9cbce1421caf88225a4926c
Thai <thailxag1122@gmail.com>
2025-11-10 15:22:09 +0700

    bottom

--- public/index.php
+++ public/index.php
@@ -6,8 +6,8 @@
 
     $dm = new DANHMUC();
     $danhmuc = $dm->laydanhmuc();
-    $mh = new MATHANG();    
-    $mathangnoibat = $mh->laymathangxemnhieu();
+    //$mh = new MATHANG();    
+    //$mathangnoibat = $mh->laymathangxemnhieu();
 
     if(isset($_REQUEST["action"])){
         $action = $_REQUEST["action"];
@@ -18,31 +18,31 @@
 
     switch($action){
         case "null": 	
-            $mathang = $mh->laymathang();
+            //$mathang = $mh->laymathang();
             include("main.php");
             break;
-        case "group":
-            if(isset($_REQUEST["id"])){
-                $id = $_REQUEST["id"];
-                $danhmuctheoID = $dm->laydanhmuctheoid($id);
-                $tendanhmuc = $danhmuctheoID['tendanhmuc'];
-                $mathang = $mh->laymathangtheodanhmuc($id);
-                include("group.php");
-            }
-            else{
-                include("main.php");
-            }
-            break;
-        case "detail":
-            if(isset($_GET["id"])){
-                $mahang = $_GET["id"];
-                $mh->tangluotxem($mahang);
-                $mhct = $mh->laymathangtheoid($mahang);
-                $madm = $mhct["danhmuc_id"];
-                $mathang = $mh->laymathangtheodanhmuc($madm);
-                include("detail.php");
-            }
-            break;
+        // case "group":
+        //     if(isset($_REQUEST["id"])){
+        //         $id = $_REQUEST["id"];
+        //         $danhmuctheoID = $dm->laydanhmuctheoid($id);
+        //         $tendanhmuc = $danhmuctheoID['tendanhmuc'];
+        //         $mathang = $mh->laymathangtheodanhmuc($id);
+        //         include("group.php");
+        //     }
+        //     else{
+        //         include("main.php");
+        //     }
+        //     break;
+        // case "detail":
+        //     if(isset($_GET["id"])){
+        //         $mahang = $_GET["id"];
+        //         $mh->tangluotxem($mahang);
+        //         $mhct = $mh->laymathangtheoid($mahang);
+        //         $madm = $mhct["danhmuc_id"];
+        //         $mathang = $mh->laymathangtheodanhmuc($madm);
+        //         include("detail.php");
+        //     }
+        //     break;
         default:
             break;
     }
